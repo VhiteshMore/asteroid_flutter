8cc83f788504e76c7a1ffa1a9df3b10689f96c28
vhitesh <vhiteshm17@gmail.com>
2024-12-11 22:04:30 +0530

    -- todo

--- lib/ui/feature/game/bloc/game_bloc.dart
+++ lib/ui/feature/game/bloc/game_bloc.dart
@@ -2,6 +2,7 @@ import 'package:asteroid_flutter/models/asteroid.dart';
 import 'package:asteroid_flutter/models/game.dart';
 import 'package:asteroid_flutter/models/particle.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
 import '../../../../models/player.dart';
@@ -13,6 +14,7 @@ class GameBloc extends ChangeNotifier {
   final Player player;
   late final GameTicker _gameTicker;
   List<Asteroid> _asteroids = [];
+  final GlobalKey gameScreenKey = GlobalKey();
 
   bool _gameStarted = false;
 
@@ -44,6 +46,33 @@ class GameBloc extends ChangeNotifier {
   set asteroids(List<Asteroid> asteroids) {
     _asteroids = asteroids;
     notifyListeners();
+  }
+
+  //Initialize particles
+  void initializeParticles() {
+
+  }
+
+  //Update particle(Asteroid & Projectiles) position based on velocity & direction on gameTicker callback
+  void updateParticlePosition() {
+
+  }
+
+  //Check if particles have left the gameScreen viewport in all directions and
+  // randomly generate new particles based on the limit of asteroids allowed
+  void verifyParticleInGamePort() {
+
+  }
+
+  //Detect collision between players and asteroids based on the detection radius
+  // around the player.
+  void detectPlayerAsteroidCollision() {
+
+  }
+
+  //Detect collision between Asteroids and Projectiles
+  void detectAsteroidProjectileCollision() {
+
   }
 
 }
